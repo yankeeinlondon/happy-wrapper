868df5de7fa8a9192644a861c6817d1da1395295
Ken Snyder <ken@ken.net>
2022-12-09 10:51:19 -0800

    chore: update to vite 4

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,13 +3,6 @@ import { defineConfig } from "vite";
 
 // used for testing, library code uses TSUP to build exports
 export default defineConfig({
-  test: {
-    dir: "test",
-    include: ["**/*.{test,spec}.{ts,mts,tsx}"],
-    environment: "happy-dom",
-    api: {
-      host: "0.0.0.0",
-    },
-  },
+
   plugins: [],
 });
