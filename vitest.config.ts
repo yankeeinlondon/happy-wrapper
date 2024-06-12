170e888b1a19a4c713f747cf4da95fdfcb68ec61
Ken Snyder <ken@ken.net>
2024-06-12 12:49:45 -0700

    fix: changed to using happy-dom-without-node to allow for use in the browser

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,13 @@
 /// <reference types="vitest" />
+import path from 'pathe'
 import { defineConfig } from "vite";
 
 // used for testing, library code uses TSUP to build exports
 export default defineConfig({
 
   plugins: [],
+  resolve: {
+    
+  },
+  
 });
